3b646ed23fb6ffa51310ba8f8af4d861c6360fec
mtijani18@outlook.com <mtijani18@outlook.com>
2025-01-03 11:40:21 +0100

    Make it ready for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/<mtijani>/' : '/',
   transpileDependencies: true,
   chainWebpack: config => {
     // Handle pdf.worker.js with worker-loader
